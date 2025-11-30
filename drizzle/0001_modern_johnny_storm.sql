a249853507bee5f8004b091950ee6341a1af4ec5
Tim Burkert <burkert.tim@gmail.com>
2025-11-30 02:07:48 +0100

    Fix?

--- drizzle/0001_modern_johnny_storm.sql
+++ drizzle/0001_modern_johnny_storm.sql
@@ -1,5 +1,3 @@
-CREATE ROLE "anon";--> statement-breakpoint
-CREATE ROLE "authenticated";--> statement-breakpoint
 ALTER TABLE "account" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 ALTER TABLE "session" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
 ALTER TABLE "user" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
